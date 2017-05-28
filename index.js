ff531718ed7aaad2bd2bb3a95fdaf01c1885d245
ZeusTheTrueGod <l-2732@yandex.ru>
2017-05-28 16:47:21 +0300

    Fix for an empty import!

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function ({ types: t }) {
     visitor: {
       ImportDeclaration (path) {
         const { node } = path;
-        if (node.specifiers.every(importNode => importNode.importKind === 'type')) {
+        if (node.specifiers.length > 0 && node.specifiers.every(importNode => importNode.importKind === 'type')) {
           path.remove();
         }
       },
